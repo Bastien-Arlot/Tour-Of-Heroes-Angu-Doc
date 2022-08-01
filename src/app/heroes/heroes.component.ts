2ec74197b5b98ee88b229c082b4b779917d894ae
Bastien-Arlot <arlot.b@gmail.com>
2022-08-01 09:42:50 +0200

    advance on tour heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,14 +10,20 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 	selectedHero?: Hero = { id: 1, name: 'Windstorm'};
 
-	heroes = HEROES;
-  constructor() { }
+	heroes: Hero[] = [];
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+	this.getHeroes();
   }
 
   onSelect(hero : Hero) {
 	this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+	this.heroService.getHeroes()
+	.subscribe(heroes => this.heroes = heroes);
   }
 
 }
