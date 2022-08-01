45597308dcc145df818b4b9e611fef66ecdc99e4
Bastien-Arlot <arlot.b@gmail.com>
2022-08-01 10:44:47 +0200

    end of routing task

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,5 +18,11 @@ export class HeroService {
 	return heroes;
   }
 
+  getHero(id: number): Observable<Hero> {
+	const hero = HEROES.find(hero => hero.id === id)!;
+	this.messageService.add(`HeroService: fetched hero id=${id}`);
+	return of(hero);
+  }
+
   
 }
